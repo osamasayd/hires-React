dd1f82f5893d22463cdfac797c0a6b0764c3592d
osamasayd <osamasayd141@gmail.com>
2023-12-08 09:20:52 +0300

    create Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import { ClassNames } from "@emotion/react";
+import './Header.css';
+
+
+function Header() {
+    return (
+        <div className="header">
+            <h2>Boost Images Resolution</h2>
+        </div>
+    )
+}
+
+export default Header;
