b778bddc4dc98316d2d1d0b612e64263ff06d36c
osamasayd <osamasayd141@gmail.com>
2023-12-08 09:23:58 +0300

    set App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
+      <Header />
+
+
+      <Footer />
     </>
   )
 }
