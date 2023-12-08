c2a7901b9ce7ad6c1aaf491c334d61f912975fbc
osamasayd <osamasayd141@gmail.com>
2023-12-08 09:21:55 +0300

    create Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <Typography variant="body1" color="inherit">
+                © 2023 OSS & Mohamad All rights reserved.
+            </Typography>
+        </div>
+    );
+};
+
+export default Footer;
